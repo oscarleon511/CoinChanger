50a6db8d00dee2a5f1cce610b61a0b8c6843bd04
oscar leon <oscarleon511@gmail.com>
2016-06-23 18:04:40 -0400

    CoinChanger Project

---
+++ ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        private static CoinChanger coins;
+
+
+        static void Main(string[] args)
+        {
+            coins = new CoinChanger();
+        }
+    }
+}
